20030d59635136a6ae92caa85b6247dec2644bd1
Soki Sakurai <sakurai@juno.phys.s.u-tokyo.ac.jp>
2012-10-01 16:45:56 +0300

    Enabled to get gsonxb_ver2.5/2.6

--- astrophysics/suzaku_gso/gso_find_nxbfile.sh
+++ astrophysics/suzaku_gso/gso_find_nxbfile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #20090507 S. Yamada
+#20121001 S. Sakurai
 
 if [ _$1 = _ ];
 then
@@ -12,7 +13,7 @@ fi
 file=$1
 
 
-tunedurl=http://www.astro.isas.jaxa.jp/suzaku/analysis/hxd/gsonxb/gsonxb_ver2.4/
+#tunedurl=http://www.astro.isas.jaxa.jp/suzaku/analysis/hxd/gsonxb/gsonxb_ver2.4/
 
 #echo "--------------------"
 #echo $file 
@@ -23,8 +24,21 @@ dateobs=`date_obs.sh $file`
 
 year=`ruby -e "print ARGV[0].split('-')[0]" $dateobs`
 month=`ruby -e "print ARGV[0].split('-')[1]" $dateobs`
+day=`ruby -e "print ARGV[0].split('-')[2]" $dateobs`
+
+if [ ${year} -lt 2011 ]; then
+tunedurl=http://www.astro.isas.jaxa.jp/suzaku/analysis/hxd/gsonxb/gsonxb_ver2.5/
+elif [ ${year} -gt 2011 ]; then
+tunedurl=http://www.astro.isas.jaxa.jp/suzaku/analysis/hxd/gsonxb/gsonxb_ver2.6/
+elif [ ${month} -lt 11 ]; then
+#hereafter year==2011
+tunedurl=http://www.astro.isas.jaxa.jp/suzaku/analysis/hxd/gsonxb/gsonxb_ver2.5/
+elif [ ${month} = 11 && ${day} -lt 4 ]; then
+tunedurl=http://www.astro.isas.jaxa.jp/suzaku/analysis/hxd/gsonxb/gsonxb_ver2.5/
+else
+tunedurl=http://www.astro.isas.jaxa.jp/suzaku/analysis/hxd/gsonxb/gsonxb_ver2.6/
+fi
 
 #echo "searching GSO $type NXB for OBSID:$obsid (${year}-${month})"
 
 echo "$tunedurl/${year}_${month}/ae${obsid}_hxd_gsobgd.evt.gz"
-
