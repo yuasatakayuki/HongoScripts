91582dc29d46512a55d39fe7dcaed73a1cb8d367
Takayuki Yuasa <yuasatakayuki@gmail.com>
2013-04-27 14:48:58 +0900

    pasjps was newly added as a command

--- astrophysics/ftoolswrapper/fits_has_header_keyword.sh
+++ astrophysics/ftoolswrapper/fits_has_header_keyword.sh
@@ -22,7 +22,8 @@ keyword=`ruby -e "print '$2'.upcase"`
 
 for n in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
 line=""
-line=`getheader.sh $file $n $keyword`
+line=`getheader.sh $file $n $keyword | sed -e "s/ /_/g"`
+
 if [ _$line != _ ]; then
 echo $n
 exit
