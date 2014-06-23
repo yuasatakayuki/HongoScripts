3e61d9b6f56328bbde880a3ae355f1a2a7aa50c2
Takayuki Yuasa <yuasatakayuki@gmail.com>
2014-06-23 16:27:21 +0900

    rename-related script and list files were removed

--- createAliasCommands.rb
+++
@@ -1,6 +0,0 @@
-open("rename_list.text").each(){|line|
-	array=line.split(" ")
-	oldName=array[0]
-	newName=array[1]
-	puts "alias #{oldName}=#{newName}"
-}
--- go_renameScriptsPreviewAsHTML.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-# Takayuki Yuasa 20140622
-
-for file in `ls bin/hs*`; do
-	echo "Processing $file"
-	outputFile=`basename $file`.html
-	ruby renameScriptsPreviewAsHTML.rb $file > html/$outputFile
-done
